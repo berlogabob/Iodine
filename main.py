2bf4dd9b1cb0e524562da22fdf99eb68bb342814
Andrew Diakov <berloga.bob@gmail.com>
2016-05-17 10:18:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 acid_list=[]
+###
 
 
 
 
 def acidls():
-    summ=input("enter: ")
+    summ = one_acid()
     acid_list.append(summ)
     print(acid_list)
-    acidls()
+    ###
 def one_acid():
     N = float(input("Какое количесто двойных связей в жирной кислоте? "))
     print("Количесто двойных связей в жирной кислоте %g " % (N))
@@ -18,23 +19,25 @@ def one_acid():
     print("\n", "\n", "\n")
     odna_kislota = (254 * N * Wi) / Mi
     print("Йодное число этой одной жирной кислоты %g " % (odna_kislota))
-
-
+    return(odna_kislota)
+    ###
 def question():
     answer = input(" Добавить значения для жирной кислоты?  (да или нет)")
     if answer == "да" or answer == "Да" or answer == "д" or answer == "ДА" or answer == "Д":
-        one_acid()
+        acidls()
         question()
+        ###
         # добавить список с значениями, которые будут считаться в след пункте
     elif answer == "нет" or answer == "Нет" or answer == "Н" or answer == "н" or answer == "НЕТ":
         print("Йодовое число равно: ")
-        #вставить расчет суммы йодового числа
+        print(sum(acid_list))
+        ###
     else:
         print("\n \n введено некорректное значение")
         print("\n")
         question()
+        ###
 question()
-
-
-
+acidls()
+###
 input("Для выхода нажмите клавишу Enter")
